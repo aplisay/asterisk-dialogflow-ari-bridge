fb42a36ed40eb5b32a9c9f7e3c5dd2304041e43f
Rob Pickering <rob@pickering.org>
2020-11-18 18:59:10 +0000

    Get config from enviroment not terribly happy with this as it is a gratuitous patch on the upstream, but means we can use it without wrapping in yet another repo/container

--- config/default.js
+++ config/default.js
@@ -1,22 +1,22 @@
 module.exports = {
-    ari: {
-        url: 'http://asterisk-fqdn:8088',
-        username: 'username',
-        password: 'foo',
-        appName: 'dialogflow'
-    },
-    rtpServer: {
-        host: 'rtp-audioserver-ip',
-        port: '7777',
-        format: 'slin16'
-    },
-    asterisk: {
-        // If set then Playback this Asterisk sound file before bridging to the dialogflow media.
-        // Useful for debug and breaking media stand-offs.
-        // playback: 'silence/1'
-    },
-    mqtt: {
-        url: 'mqtt://test.mosquitto.org',
-        topicPrefix: 'dialogflow-asterisk'
-    }
+  ari: {
+    url: 'http://asterisk:8080',
+    username: process.env.ARI_USER,
+    password: process.env.ARI_PASSWORD,
+    appName: process.env.ARI_APPLICATION
+  },
+  rtpServer: {
+    host: 'audioserver',
+    port: 7777,
+    format: 'slin16'
+  },
+  mqtt: {
+    url: 'mqtt://mqtt',
+    topicPrefix: 'dialogflow-asterisk'
+  },
+  asterisk: {
+    // If set then Playback the Asterisk sound file specified before bridging to the dialogflow media.
+    // Useful for debug and breaking media stand-offs.
+    playback: 'silence/1'
+  },
 }
